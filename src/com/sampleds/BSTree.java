675ade3214360c86f30d0aa57607ad0f698376f3
ADITYA PRASANN <aditya.prasann@gmail.com>
2018-08-26 20:49:28 +0800

    improvements

--- src/com/sampleds/BSTree.java
+++ src/com/sampleds/BSTree.java
@@ -25,27 +25,27 @@ public class BSTree {
     }
 
     private void displayInOrder(Node root) {
-        if (root != null) {
-            displayInOrder(root.lTree);
-            System.out.print(root.payLoad + " ");
-            displayInOrder(root.rTree);
-        }
+        if (root == null)
+            return;
+        displayInOrder(root.lTree);
+        System.out.print(root.payLoad + " ");
+        displayInOrder(root.rTree);
     }
 
     private void displayPreOrder(Node root) {
-        if (root != null) {
-            System.out.print(root.payLoad + " ");
-            displayInOrder(root.lTree);
-            displayInOrder(root.rTree);
-        }
+        if (root == null)
+            return;
+        System.out.print(root.payLoad + " ");
+        displayPreOrder(root.lTree);
+        displayPreOrder(root.rTree);
     }
 
     private void displayPostOrder(Node root) {
-        if (root != null) {
-            displayInOrder(root.lTree);
-            displayInOrder(root.rTree);
-            System.out.print(root.payLoad + " ");
-        }
+        if (root == null)
+            return;
+        displayPostOrder(root.lTree);
+        displayPostOrder(root.rTree);
+        System.out.print(root.payLoad + " ");
     }
 
     private boolean search(int id) {
@@ -60,6 +60,22 @@ public class BSTree {
             }
         }
         return false;
+    }
+    private boolean isValidBST(Node root, int minValue, int maxValue) {
+        if (root == null)
+            return true;
+        if(root.payLoad > minValue && root.payLoad < maxValue && isValidBST(root.lTree,minValue,root.payLoad) && isValidBST(root.rTree, root.payLoad, maxValue))
+            return true;
+        else
+            return false;
+    }
+
+    private int getHeightBST(Node root) {
+        if(root == null)
+            return 0;
+        int lHeight = getHeightBST(root.lTree);
+        int rHeight = getHeightBST(root.rTree);
+        return 1 + Math.max(lHeight,rHeight);
     }
 
     public static void main(String[] args) {
@@ -80,9 +96,16 @@ public class BSTree {
         System.out.println("Post Order Original Tree : ");
         b.displayPostOrder(b.root);
         System.out.println();
+        System.out.println("Search for 24");
         System.out.println(b.search(24));
+        System.out.println("Search for 15");
         System.out.println(b.search(15));
+        System.out.println("Is It a Valid BST? ");
+        System.out.println(b.isValidBST(b.root,Integer.MIN_VALUE,Integer.MAX_VALUE));
+        System.out.println("height of BST is");
+        System.out.println(b.getHeightBST(b.root));
     }
+
 
     class Node {
         private int payLoad;
